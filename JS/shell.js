a2681dceb71987b0a5f8701ef6fbf2694a38868d
Paul BONCAR <80626421+n0ct3ri4@users.noreply.github.com>
2021-03-31 08:25:32 +0400

    Add files via upload

--- JS/shell.js
+++ JS/shell.js
@@ -1,10 +1,9 @@
 // Handleable program.
 
 const { exec } = require("child_process");
-const { argv } = require("process");
 
-exec(argv.slice(2).join(" "), (err, stdout, stderr) => {
-  console.log(argv.slice(2).join(" "));
+exec(process.argv.slice(2).join(" "), (err, stdout, stderr) => {
+  console.log(process.argv.slice(2).join(" "));
   if (err) throw err;
   if (stdout) console.log(stdout);
   if (stderr) console.log(stderr);
