e4190f320bcccc7c3243974b27b9159a47a61c1c
Paul BONCAR <80626421+n0ct3ri4@users.noreply.github.com>
2021-03-31 08:28:25 +0400

    Updating some files.

---
+++ build/CMakeFiles/ihe.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ihe.dir/Main.cpp.o"
+  "ihe"
+  "ihe.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ihe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/ihe.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ihe.
+# This may be replaced when dependencies are built.
